e8a4f3eb15596347d34f84faf622257790e6d2ff
JavierDossmanCobos <dossmanj@gmail.com>
2023-06-15 17:54:52 -0500

    Updated on 16 June 2023 @5:54pm

--- Actividades/app/src/main/java/com/usa/actividades/MainActivity.java
+++ Actividades/app/src/main/java/com/usa/actividades/MainActivity.java
@@ -2,19 +2,34 @@ package com.usa.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnSiguiente;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSiguiente = findViewById(R.layout.btnSiguiente);
-    }
+        btnSiguiente = findViewById(R.id.btnSiguiente);
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                siguienteActividad();
+            }
+        });
 }
+
+    private void siguienteActividad() {
+        Intent intent = new Intent(MainActivity.this, SegundaActivity.class);
+        //Todo  enviar datos
+        intent.putExtra("nombreProducto", "Calzado Adidas");
+        intent.putExtra("precioProducto",35000000);
+        startActivity(intent);
+
+    }
+    }
--- Actividades/app/src/main/java/com/usa/actividades/SegundaActivity.java
+++ Actividades/app/src/main/java/com/usa/actividades/SegundaActivity.java
@@ -2,13 +2,28 @@ package com.usa.actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SegundaActivity extends AppCompatActivity {
 
+    TextView txtNombreProducto, txtPrecioProducto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        txtNombreProducto = findViewById(R.id.txtNombreProducto);
+        txtPrecioProducto = findViewById(R.id.txtPrecioProducto);
+        Bundle datos = getIntent().getExtras();
+
+        String nombre = datos.getString("nombreProducto");
+        int precio = datos.getInt("precioProducto");
+
+        txtNombreProducto.setText(nombre);
+        txtPrecioProducto.setText("$"+String.valueOf(precio));
+
     }
+
 }
