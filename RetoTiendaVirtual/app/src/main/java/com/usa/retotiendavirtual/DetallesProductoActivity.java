1c12cbc43058418c60325238f66b189153d306b3
JavierDossmanCobos <dossmanj@gmail.com>
2023-06-16 06:18:48 -0500

    Updated on 16 June 2023 @6:18am - RetoTiendaVirtual

--- RetoTiendaVirtual/app/src/main/java/com/usa/retotiendavirtual/DetallesProductoActivity.java
+++ RetoTiendaVirtual/app/src/main/java/com/usa/retotiendavirtual/DetallesProductoActivity.java
@@ -3,12 +3,32 @@ package com.usa.retotiendavirtual;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetallesProductoActivity extends AppCompatActivity {
+
+    ImageView imgDetalleProducto;
+    TextView txtNombre, txtDescripcion, txtPrecio, txtCantidad;
+    Button btnResCantidad, btnSumCantidad, btnAgregarProducto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_producto);
+
+        imgDetalleProducto = findViewById((R.id.imgDetalleProducto));
+        txtNombre = findViewById((R.id.txtNombreDetalleProducto));
+        txtDescripcion = findViewById((R.id.txtDescripcionDetalleProducto));
+        txtPrecio = findViewById((R.id.txtPrecioDetalleProducto));
+        txtCantidad = findViewById((R.id.textCantidad));
+        btnResCantidad = findViewById((R.id.btnResCantidad));
+        btnSumCantidad = findViewById((R.id.btnSumCantidad));
+        btnAgregarProducto = findViewById((R.id.btnAgregarProducto));
+
+        //cargarDatos();
+
+
     }
 }
--- RetoTiendaVirtual/app/src/main/java/com/usa/retotiendavirtual/ui/producto/adapter/ProductoAdapter.java
+++ RetoTiendaVirtual/app/src/main/java/com/usa/retotiendavirtual/ui/producto/adapter/ProductoAdapter.java
@@ -1,14 +1,18 @@
 package com.usa.retotiendavirtual.ui.producto.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.usa.retotiendavirtual.DetallesProductoActivity;
 import com.usa.retotiendavirtual.R;
 import com.usa.retotiendavirtual.ui.producto.model.ProductoModel;
 
@@ -40,10 +44,10 @@ public class ProductoAdapter extends RecyclerView.Adapter<ProductoAdapter.Produc
         holder.txtDescripcionProducto.setText(productos.get(position).getDescripcion());
         holder.txtPrecioProducto.setText("$ "+String.valueOf(productos.get(position).getPrecio()));
         holder.imgProducto.setImageResource(productos.get(position).getImagen());
-
+        holder.posicion = holder.getAdapterPosition();
     }
     public class ProductoViewHolder extends RecyclerView.ViewHolder {
-
+        int posicion;
         ImageView imgProducto;
         TextView txtTituloProducto, txtDescripcionProducto, txtPrecioProducto;
         public ProductoViewHolder(@NonNull View itemView) {
@@ -53,6 +57,20 @@ public class ProductoAdapter extends RecyclerView.Adapter<ProductoAdapter.Produc
             txtTituloProducto = itemView.findViewById(R.id.txtTituloProducto);
             txtDescripcionProducto = itemView.findViewById(R.id.txtDescripcionProducto);
             txtPrecioProducto = itemView.findViewById(R.id.txtPrecioProducto);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                //    Toast.makeText(itemView.getContext(), "Click en un card view  "+String.valueOf(posicion),Toast.LENGTH_SHORT).show();
+
+                    Context contexto = itemView.getContext();
+                    Intent intent = new Intent(itemView.getContext(), DetallesProductoActivity.class);
+                    //Todo Aqui debemos mandar los datos a la otra actividad
+                    contexto.startActivity(intent);
+
+                }
+            });
+
         }
     }
 
